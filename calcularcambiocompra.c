32f43a5f85f18d49325ec350796e8c8455e1764f
phkandresc <kevin.coraizaca@ucuenca.edu.ec>
2023-09-25 21:24:05 -0500

    cambios importantes hechos

--- calcularcambiocompra.c
+++ calcularcambiocompra.c
@@ -1,42 +1,46 @@
-
 #include <stdio.h>
 #include <math.h>
+double denominacionDolar[9] = {20, 10, 5, 1, 0.50, 0.25, 0.10, 0.05, 0.01};
+double cantidadporDenominacion[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int main()
 {
-    float vprod, vprodpagado, vuelto;
-    float denominacionDolar[9] = {20, 10, 5, 1, 0.50, 0.25, 0.10, 0.05, 0.01};
-    float cantidadporDenominacion[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    double vprod, vprodpagado;
+    double vuelto;
 
     printf("Ingrese el valor a pagar por el cliente:\n");
-    scanf("%f", &vprod);
+    scanf("%lf", &vprod);
     printf("Ingrese el valor pagado por el cliente:\n");
-    scanf("%f", &vprodpagado);
+    scanf("%lf", &vprodpagado);
 
-    if (vprodpagado==vprod)
+    if (vprodpagado == vprod)
     {
         printf("Pago realizado con exito, sin vuelto\n");
     }
 
-    if (vprodpagado > vprod){
-        // calcular vuelto
-        vuelto = vprodpagado-vprod;
-        vuelto = floor(vuelto*100)/100;
-        printf("Debe entregar un cambio de: $%f\n", vuelto);
-        // billetes
-        for (int i = 0; i<9; i++)
+    if (vprodpagado > vprod)
+    {
+        // Calcular vuelto
+        vuelto = vprodpagado - vprod;
+        printf("Debe entregar un cambio de: %f\n", vuelto);
+
+        // Vuelto segun denominaciones
+        for (int i = 0; i < 9; i++)
         {
-            while (vuelto>=denominacionDolar[i])
+            while (vuelto >= denominacionDolar[i])
             {
-                vuelto=vuelto-denominacionDolar[i];
-                printf("el vuelto es: %f\n", vuelto);
+                vuelto = vuelto - denominacionDolar[i];
                 cantidadporDenominacion[i]++;
             }
-            printf("Billetes de %.2f\t Cantidad %.0f\n", denominacionDolar[i], cantidadporDenominacion[i]);
         }
     }
     else
     {
-        printf("Pago Insuficiente\n");
+        printf("Pago insuficiente\n");
     }
 }
+
+/*            for (int j = 0; cantidadporDenominacion[j] > 0 && j < 9; j++)
+            {
+                printf("Billetes de %.0f\t Cantidad: %.0f\n", denominacionDolar[j], cantidadporDenominacion[j]);
+            }*/
