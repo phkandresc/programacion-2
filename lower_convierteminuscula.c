901e1a9ee2c2df63f1d0fb3640ba59f2d2e8ec6a
phkandresc <kevin.coraizaca@ucuenca.edu.ec>
2023-09-30 23:43:16 -0500

    se usa expresiones condicionales para reescribir

--- lower_convierteminuscula.c
+++ lower_convierteminuscula.c
@@ -3,11 +3,16 @@
 int lower(int c);
 
 int main(){
-    printf("Caracter convertido a minuscula %d", lower(90));
+    printf("Caracter convertido a minuscula %d", lower(67));
 }
 
 int lower(int c){
-    if(c>='A' && c<='Z'){
+    /*if(c>='A' && c<='Z'){
         return c +'a'-'A';
     }else return c;
+    Aqui se usa if- else como cualquiera haria
+    pero la expresion de abajo simplifica este proceso.
+    */
+    
+    return((c>='A' && c<='Z')? c +'a'-'A':c);
 }
