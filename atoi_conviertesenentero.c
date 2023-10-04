1476329f86b98376cfec0de197023c704983e80d
phkandresc <kevin.coraizaca@ucuenca.edu.ec>
2023-10-03 21:52:23 -0500

    fsdf

--- atoi_conviertesenentero.c
+++ atoi_conviertesenentero.c
@@ -2,16 +2,19 @@
 int atoi(char s[]);
 void get_line(char s[], int maxline);
 
-int main(){
-    char s[10] = {'0','2','0', '0','5'};
+int main()
+{
+    char s[10] = {'0', '2', '0', '0', '5'};
     printf("Numero en entero int %i", atoi(s));
 }
 
-int atoi(char s[]){
+int atoi(char s[])
+{
     int i, n;
     n = 0;
-    for(i=0; s[i]>='0' && s[i]<='9';++i){
-        n=10*n+(s[i]-'0');
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+    {
+        n = 10 * n + (s[i] - '0');
     }
     return n;
 }
